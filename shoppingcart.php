3c6963f496cf32732645fbb42fc7fb9125eed074
Serge <sergeifrolov@hotmail.nl>
2020-11-12 11:23:59 +0100

    shoppingCart

--- shoppingcart.php
+++ shoppingcart.php
@@ -3,25 +3,79 @@ $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
 mysqli_set_charset($Connection, 'latin1');
 include __DIR__ . "/header.php";
 
-include "CartFuncties.php";
-$cart = GetCart();
-print_r($cart);
 ?>
 
 <div>
 
-<table class="tbl-cart" cellpadding="10" cellspacing="1">
-    <tbody>
-    <tr>
-        <th style="text-align:left;" name="Productname">Productnaam</th>
-    </tr>
-    <tr>
-        <th style="text-align:left; name="Description">Beschrijving</th>
-        <th style="text-align:right width="5%";">Aantal</th>
-        <th style="text-align:right width="10%";">Prijs</th>
+    <header>
+    <h2 style="text-align: left;">Winkelwagen</h2>
+        <h6 style="text-align:right;">Aanvullende beschrijving</h6>
+    </header>
+
+    <style>
+
+    </style>
+
+    <hr color="white">
+    <br>
+
+    <table class="tbl-cart" cellpadding="10" cellspacing="5">
+        <tbody>
+        <tr>
+            <td>
+             <img border="5" src="Public\ProductIMGHighRes\mug.png" width="100" height="100" alt="kop">
+            </td>
+
+            <td style="text-align:left;" name="Productname">Productnaam<br>Beschrijving</td>
+
+            <td style="text-align:left;">Verzend Informatie<br>Aanvullende Informatie</td>
+
+            <td><label for="aantal">Selecteer aantal:</label></td>
+
+            <td><select class="" name="Aantal" id="Aantal">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select></td>
+
+            <td style="text-align:right;">Prijs:</td>
     </tr>
     </tbody>
-</table>
+    </table>
+
+    <br>
+    <hr color="white">
 
 </div>
+    <div style="width:20%;">
+        <form method="GET" action="CartFuncties.php">
+            <td><label for="kortingsbon">Vul hier je kortingscode in (optioneel):</label>
+                <input type="text" id="kortingsbon" name="kortingsbon" value="kortingsbon"></td><br>
+        </form>
+    </div>
 
+<table style="text-align:right">
+    <td style="text-align:right;" name="Totaal">Totaal Artikelen</td>
+    <td style="text-align:right;" name="Prijs1">Prijs</td>
+</table>
+
+<table>
+    <td style="text-align:right;" name="Verzendkosten">Verzendkosten</td>
+    <td style="text-align:right;" name="Prijs2">Prijs</td>
+</table>
+
+    <hr color="white" style="width:10%;">
+
+<table>
+    <td style="text-align:right;" name="Totaal">Totaal Artikelen</td>
+    <td style="text-align:right;" name="Prijs3">Prijs</td>
+    </td>
+</table>
+
+<?php
+include "CartFuncties.php";
+$cart = GetCart();
+print_r($cart);
+?>
