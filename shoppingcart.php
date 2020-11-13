a5fa6336a099abe75107d1fa91a2bd6c16c192d8
Justin Passchier <juspasschier@gmail.com>
2020-11-13 10:57:04 +0100

    Added foreach for items in cart

--- shoppingcart.php
+++ shoppingcart.php
@@ -2,13 +2,16 @@
 $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
 mysqli_set_charset($Connection, 'latin1');
 include __DIR__ . "/header.php";
+include "CartFuncties.php";
+$cart = GetCart();
+print_r($cart);
 
 ?>
 
 <div>
 
     <header>
-    <h2 style="text-align: left;">Winkelwagen</h2>
+        <h2 style="text-align: left;">Winkelwagen</h2>
         <h6 style="text-align:right;">Aanvullende beschrijving</h6>
     </header>
 
@@ -18,43 +21,45 @@ include __DIR__ . "/header.php";
 
     <hr color="white">
     <br>
+    <?php foreach ($cart as $item) { ?>
+        <table class="tbl-cart" cellpadding="10" cellspacing="5">
+            <tbody>
+            <tr>
+                <td>
+                    <img border="5" src="Public\ProductIMGHighRes\mug.png" width="100" height="100" alt="kop">
+                </td>
 
-    <table class="tbl-cart" cellpadding="10" cellspacing="5">
-        <tbody>
-        <tr>
-            <td>
-             <img border="5" src="Public\ProductIMGHighRes\mug.png" width="100" height="100" alt="kop">
-            </td>
+                <td style="text-align:left;" name="Productname">Productnaam<br>Beschrijving</td>
 
-            <td style="text-align:left;" name="Productname">Productnaam<br>Beschrijving</td>
+                <td style="text-align:left;">Verzend Informatie<br>Aanvullende Informatie</td>
 
-            <td style="text-align:left;">Verzend Informatie<br>Aanvullende Informatie</td>
+                <td><label for="aantal">Selecteer aantal:</label></td>
 
-            <td><label for="aantal">Selecteer aantal:</label></td>
+                <td><select class="" name="Aantal" id="Aantal">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select></td>
 
-            <td><select class="" name="Aantal" id="Aantal">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select></td>
-
-            <td style="text-align:right;">Prijs:</td>
-    </tr>
-    </tbody>
-    </table>
+                <td style="text-align:right;">Prijs:</td>
+            </tr>
+            </tbody>
+        </table>
+    <?php } ?>
 
     <br>
     <hr color="white">
 
 </div>
-    <div style="width:20%;">
-        <form method="GET" action="CartFuncties.php">
-            <td><label for="kortingsbon">Vul hier je kortingscode in (optioneel):</label>
-                <input type="text" id="kortingsbon" name="kortingsbon" value="kortingsbon"></td><br>
-        </form>
-    </div>
+<div style="width:20%;">
+    <form method="post" action="CartFuncties.php">
+        <td><label for="kortingsbon">Vul hier je kortingscode in (optioneel):</label>
+            <input type="text" id="kortingsbon" name="kortingsbon" value="kortingsbon"></td>
+        <br>
+    </form>
+</div>
 
 <table style="text-align:right">
     <td style="text-align:right;" name="Totaal">Totaal Artikelen</td>
@@ -66,7 +71,7 @@ include __DIR__ . "/header.php";
     <td style="text-align:right;" name="Prijs2">Prijs</td>
 </table>
 
-    <hr color="white" style="width:10%;">
+<hr color="white" style="width:10%;">
 
 <table>
     <td style="text-align:right;" name="Totaal">Totaal Artikelen</td>
@@ -74,8 +79,3 @@ include __DIR__ . "/header.php";
     </td>
 </table>
 
-<?php
-include "CartFuncties.php";
-$cart = GetCart();
-print_r($cart);
-?>
