219c3631995b2b15bdd0e239817c1b6d85db7b46
Justin Passchier <juspasschier@gmail.com>
2020-11-16 12:25:34 +0100

    Fixed old merge conflict still in file

--- cart.php
+++ cart.php
@@ -2,24 +2,10 @@
 include "CartFuncties.php";
 session_start();
 $cart = GetCart();
-<<<<<<< HEAD
-foreach ($cart as $product => $aantal) {
-    print("product: " . $product . " aantal: " . $aantal . "<br>");
-
-
-}
-
-ProductVooraad();
-=======
 print_r($cart);
->>>>>>> development
-
 //gegevens per artikel (naam, prijs, etc.) in $cart-array  uit database halen
 //totaal prijs berekeken
 //winkelmandje weergeven in html (bv. met tabel)
 
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> development
+
