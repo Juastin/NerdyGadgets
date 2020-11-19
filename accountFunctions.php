d9384a287f882b21509d31cdfdd6a32f1244b6f7
Justin <juspasschier@gmail.com>
2020-11-19 13:09:05 +0100

    Added new field names and began on form handling, also changed database by making middlename can be null

--- accountFunctions.php
+++ accountFunctions.php
@@ -1,9 +1,21 @@
 <?php
-function GetSingleImage ($Connection, $id) {
+function Register ($Connection, $post) {
+    $firstname = $post['firstname'];
+    $middlename = $post['middlename'];
+    $lastname = $post['lastname'];
+    $password = $post['password'];
+    $postalcode = $post['postalcode'];
+    $email = $post['email'];
+    $city = $post['city'];
+    $address = $post['address'];
+    $tel = $post['tel'];
     $Query = "
-    SELECT ImagePath
-    FROM stockitemimages
-    WHERE StockItemID = ?";
+    INSERT INTO user (firstName, middleName, lastName, password, postalCode, email, city, address, tel) 
+    VALUES ('".$firstname."',
+    '".$middlename."','".$lastname."',
+    '".$password."','".$postalcode."',
+    '".$email."','".$city."',
+    '".$address."','".$tel."',)";
     $Statement = mysqli_prepare($Connection, $Query);
     mysqli_stmt_bind_param($Statement, "i", $id);
     mysqli_stmt_execute($Statement);
--- register.php
+++ register.php
@@ -3,24 +3,47 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head><?php
 include __DIR__ . "/header.php";
+include "accountFunctions.php";
 $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets_simple"); ?>
 <div class="row">
     <div class="col-2"></div>
-    <form method="post" class="form-group col-8">
+    <form method="post" class="form-group col-8" >
         <h1 class="row loginTekst">Registreren.</h1>
         <a class="row">Vul hier uw gegevens in om te registreren.</a>
         <a class="row">Alle velden met sterretje zijn verplicht.</a>
-        <input placeholder="Voornaam*" name="firstname" class="loginInput" required>
-        <input placeholder="Tussenvoegsel*" name="middlename" class="loginInput" required>
-        <input placeholder="Achternaam*" name="lastname" class="loginInput" required>
-        <input placeholder="E-mail*" type="email" name="email" class="loginInput" required>
-        <input placeholder="Wachtwoord*" type="password" name="password" class="loginInput" required>
-        <input placeholder="Woonplaats*" name="city" class="loginInput" required>
-        <input placeholder="Adres*" name="address" class="loginInput" required>
-        <input placeholder="Postcode*" name="postalcode" class="loginInput" required>
-        <input placeholder="Telefoonnummer" name="tel" class="loginInput">
+        <label for="firstname" class="loginInput">Voornaam*</label>
+            <input placeholder="Voornaam*" name="firstname" required>
+        <label for="middlename" class="loginInput">Tussenvoegsel</label>
+            <input placeholder="Tussenvoegsel" name="middlename">
+        <label for="lastname" class="loginInput">Achternaam*</label>
+            <input placeholder="Achternaam*" name="lastname" required>
+        <label for="email" class="loginInput">E-mail*</label>
+            <input placeholder="E-mail*" type="email" name="email" required>
+        <label for="password" class="loginInput">Wachtwoord*</label>
+            <input placeholder="Wachtwoord*" type="password" name="password" required>
+        <label for="passwordcheck" class="loginInput">Wachtwoord herhalen*</label>
+            <input placeholder="Wachtwoord herhalen*" type="password" name="passwordcheck" required>
+        <label for="city" class="loginInput">Woonplaats*</label>
+            <input placeholder="Woonplaats*" name="city" required>
+        <label for="address" class="loginInput">Adres*</label>
+            <input placeholder="Adres*" name="address" required>
+        <label for="postalcode" class="loginInput">Postalcode*</label>
+            <input placeholder="Postcode*" name="postalcode" required>
+        <label for="tel" class="loginInput">Telefoonnummer</label>
+            <input placeholder="Telefoonnummer" name="tel">
         <input type="submit" class="loginInput col-2 btn btn-primary btn-outline-dark addToCartButton" value="Registreren" name="submit">
     </form>
-    <?php print_r($_POST); ?>
+    <?php
+    if (isset($_POST['submit'])) {
+        if ($_POST['password'] == $_POST['passwordcheck']) {
+            Register($Connection, $_POST);
+        }
+        else {
+            echo "<script type='text/javascript'>alert('Wachtwoorden komen niet overeen met elkaar');</script>";
+        }
+    }
+//    Register($Connection, $_POST)
+    print_r($_POST);
+    ?>
     <div class="col-2"
 </div>
