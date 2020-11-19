ae5c32c36decf3bf3f3f57c7496c3c30d45cdf60
Justin <juspasschier@gmail.com>
2020-11-19 11:54:24 +0100

    Added export file for user table in new database

--- login.php
+++ login.php
@@ -2,14 +2,16 @@
     <title>Login</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head><?php
-include __DIR__ . "/header.php"; ?>
+include __DIR__ . "/header.php";
+$Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");?>
 <div class="row">
     <div class="col-2"></div>
     <form method="post" class="form-group col-8">
-        <h3 class="row loginTekst">Vul hier uw logingegevens in.</h3>
+        <h3 class="row loginTekst">Login.</h3>
+        <a class="row">Vul hier uw logingegevens in.</a>
         <input placeholder="E-mail" name="email" class="loginInput">
         <input placeholder="Wachtwoord" name="password" class="loginInput">
-        <input type="submit" class="loginInput col-2 btn btn-primary btn-outline-dark addToCartButton" value="Login" name="submit">
+        <input type="submit" class="loginInput col-2 btn btn-primary btn-outline-dark addToCartButton" value="Inloggen" name="submit">
     </form>
     <?php print_r($_POST); ?>
     <div class="col-2"
