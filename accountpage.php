378813b4152f7a176c1f8af55cb090e4cf88d565
Justin <juspasschier@gmail.com>
2020-11-26 12:02:00 +0100

    Removed print_r and added session_start

--- accountpage.php
+++ accountpage.php
@@ -1,6 +1,8 @@
 <?php
-if (!$_SESSION['loggedIn'] == TRUE) {
+session_start();
+$_SESSION['loggedIn'];
+if ($_SESSION['loggedIn'] == false) {
+
     header('Location: login.php');
 }
 include __DIR__ . "/header.php";
-print_r($_SESSION);
