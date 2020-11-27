7998138ec7986715e7167288a6d00c150e7e499d
Serge <sergeifrolov@hotmail.nl>
2020-11-27 12:36:42 +0100

    voorraad_aanpassen_bestellen

--- afrondingBestelling.php
+++ afrondingBestelling.php
@@ -7,7 +7,7 @@ include __DIR__ . "/header.php";
 
 <body>
     <div>
-        <form method="post" action="orderpage.php">
+        <form method="post" action="afrondingBestelling.php">
             <div style="text-align: center;">
                 <a href="bestellingAfgerond.php" class="btn btn-primary btn-outline-dark font-weight-bold" style="background-color: #FFFF00;" id="afronden" role="button"> Bestelling Afronden &nbsp; <i class="fas fa-arrow-right"></i></a>
             </div>
