8783fbcf23a7ba1f96f07a216173baa8073e3e18
Serge <sergeifrolov@hotmail.nl>
2020-11-28 11:49:53 +0100

    voorraad_aanpassen_bestellen

--- bestellingAfgerond.php
+++ bestellingAfgerond.php
@@ -3,6 +3,8 @@ $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
 mysqli_set_charset($Connection, 'latin1');
 session_start();
 include __DIR__ . "/header.php";
+include "CartFuncties.php";
+include "viewFunctions.php";
 ?>
 <body style="text-align: center;">
 
