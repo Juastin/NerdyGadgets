9c1e564ed5504bf5c7be799034b99e2127c1c8a1
Justin <juspasschier@gmail.com>
2020-11-29 16:29:53 +0100

    Added some styling fixes to be wireframe depended

--- accountpage.php
+++ accountpage.php
@@ -9,7 +9,7 @@ include __DIR__ . "/header.php";
 include "accountFunctions.php";
 $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets_simple");
 $row = getInformation($Connection, $_SESSION['user']); ?>
-<div class="container">
+<div>
     <div class="row">
         <div class="col-2"></div>
         <div class="col-8">
@@ -18,30 +18,31 @@ $row = getInformation($Connection, $_SESSION['user']); ?>
         </div>
         <div class="col-2"></div>
     </div>
+    <div class="accountContainer">
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-7">
+        <div class="col-2"></div>
+        <div class="col-8">
             <i class="fas fa-user"></i>
             <a class="accountInfo">Klantennummer: <?php print($row['userId']);?></a>
         </div>
     </div>
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-7">
+        <div class="col-2"></div>
+        <div class="col-8">
             <i class="fas fa-envelope"></i>
             <a class="accountInfo"><?php print($row['email']);?></a>
         </div>
     </div>
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-7">
+        <div class="col-2"></div>
+        <div class="col-8">
             <i class="fas fa-home"></i>
             <a class="accountInfo"><?php print($row['address']." "); print($row['houseNumber'].", "); print($row['city']." "); ?></a>
         </div>
     </div>
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-7">
+        <div class="col-2"></div>
+        <div class="col-8">
             <i class="fas fa-map-marker-alt"></i>
             <a class="accountInfo"><?php print($row['postalCode'])?></a>
         </div>
