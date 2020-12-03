bb238d15b6716fc28c796d36bc615b0099fed404
YassiraRaddahi <yassira.live@live.nl>
2020-12-03 18:22:17 +0100

    winkelmandje wordt zonder verversen van de pagina direct geleegd na bestelling te hebben geplaatst

--- bestellingAfgerond.php
+++ bestellingAfgerond.php
@@ -9,17 +9,15 @@ if (isset($_SESSION['cart'])) {
     foreach ($_SESSION['cart'] as $id => $quantity){
     if (isset($_POST["afronden"])){
         UpdateVoorraad($Connection, $quantity, $id);
-        }
-    }
-    unset($_SESSION['cart']);
+        unset($_SESSION['cart']); ?>
+        <script> window.location.href = 'bestellingAfgerond.php'; </script>
+    <?php  }
+    }}
+
     ?>
+
     <body>
     <div class="container" style="text-align: center;">
-    <?php print "U heeft betaald!🎉 🎉 🙌. <br> Wij gaan meteen voor u aan de slag!👷 🛒 "; ?>
+        <?php print "U heeft betaald!🎉 🎉 🙌. <br> Wij gaan meteen voor u aan de slag!👷 🛒 "; ?>
     </div>
     </body>
-<?php
-}
-else {
-
-}
