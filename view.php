c0b2d7ccf11e70757095d98958a290579b418af7
YassiraRaddahi <yassira.live@live.nl>
2020-12-03 15:51:29 +0100

    print_r weggehaald

--- view.php
+++ view.php
@@ -112,7 +112,6 @@ $cart = GetCart();
                         $sql = "SELECT QuantityOnHand FROM stockitemholdings WHERE stockitemid=" . $Result["StockItemID"];
                         $results = mysqli_query($Connection, $sql);
                         $voorraadcheck = mysqli_fetch_assoc($results);
-                        print_r($voorraadcheck);
                         if (isset($_POST['submit'] ) && (($cart[$Result["StockItemID"]]) < $voorraadcheck['QuantityOnHand'])){
                             AddProductToCart($_POST['stockItemID']); ?>
                           <script>  window.location.href = 'view.php?id=<?php print($Result["StockItemID"])?>'; </script>
